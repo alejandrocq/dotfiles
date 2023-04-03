6abb4f02a8fdacc4f3b0987b2bd969c8b149d04a
Alejandro Castilla <contacto@alejandro-castilla.com>
2023-04-03 23:06:35 +0200

    Add nvim config, lsd alias and improvements on install script

--- install.sh
+++ install.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
+
 echo "🍻 Let's install some awesome dotfiles..."
+
+# Work on home directory
 cd ~
 
-[[ -d .oh-my-zsh ]] && echo 'oh-my-zsh already installed ✅' || CHSH=yes RUNZSH=no sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-[[ $? -ne 0 ]] && echo 'oh-my-zsh installation failed ❌' && exit 1
+if [ ! "$(command -v fzf)" ] || [ ! "$(command -v bat)" ] || [ ! "$(command -v nvim)" ] || [ ! "$(command -v lsd)" ]; then
+  echo "❌ Some of the following commands are not available: fzf, bat, nvim and lsd"
+  exit 1
+fi
 
-[[ -e .p10k.zsh ]] && echo 'powerlevel10k already installed ✅' || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-[[ $? -ne 0 ]] && echo 'powerlevel10k installation failed ❌' && exit 1
+[[ -d .oh-my-zsh ]] && echo '✅ oh-my-zsh already installed' || CHSH=yes RUNZSH=no sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+[[ $? -ne 0 ]] && echo '❌ oh-my-zsh installation failed' && exit 1
+
+[[ -e .p10k.zsh ]] && echo '✅ powerlevel10k already installed' || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+[[ $? -ne 0 ]] && echo '❌ powerlevel10k installation failed' && exit 1
 
 RESPONSE="N"
 read -rp '⚠️  Are you sure you want to delete the dotfiles and zshrc directories, and all their content? (Y/N) ' RESPONSE
 
-if [[ $RESPONSE == "Y" ]]; then
+if [ "$RESPONSE" == "Y" ]; then
 	rm -rf .dotfiles
-	git clone https://github.com/alejandrocq/dotfiles.git
-	[[ $? -ne 0 ]] && echo "Can't clone dotfiles repository ❌" && exit 1
+	git clone https://github.com/alejandrocq/dotfiles.git || (echo "Can't clone dotfiles repository ❌" && exit 1)
 	mv dotfiles .dotfiles
 
 	rm -rf .zshrc .zsh
 	ln -s .dotfiles/zsh/zshrc .zshrc
 	ln -s .dotfiles/zsh/ .zsh
 	ln -s .dotfiles/zsh/p10k.zsh .p10k.zsh
+
+	mkdir -p .config/nvim/
+	ln -s ~/.dotfiles/nvim/init.vim .config/nvim/init.vim
 
 	echo "Installation completed! Enjoy 😁"
 	exec zsh
